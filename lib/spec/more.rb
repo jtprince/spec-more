8f886d8d0b7bfd2cdb04e3a78378c85f837d8b78
John Prince <jtprince@gmail.com>
2009-11-19 13:53:35 -0700

    Version bump to 0.0.3

---
+++ lib/spec/more.rb
@@ -0,0 +1,68 @@
+require 'rubygems'
+require 'bacon'
+
+class Object
+  
+  def is(other)
+    should.equal other
+  end
+
+  def isnt(other)
+    should.not.equal other
+  end
+
+  def isa(other)
+    self.class.should.equal other
+  end
+
+  def matches(other)
+    should.match other
+  end
+
+  # element wise matching using each
+  def enums(other)
+    self_ar = []
+    self.each do |v|
+      self_ar << v
+    end
+    other_ar = []
+    other.each do |v|
+      other_ar << v
+    end
+    self_ar.zip(other_ar) do |a,b|
+      a.should.equal b
+    end
+  end
+
+  def ok
+    should.equal true
+  end
+end
+
+module Bacon
+  class Context
+    def ok(arg)
+      arg.should.equal true
+    end
+
+    def hash_match(hash, obj)
+      hash.each do |k,v|
+        if v.is_a?(Hash)
+          hash_match(v, obj.send(k.to_sym))
+        else
+          puts "#{k}: #{v} but was #{obj.send(k.to_sym)}" if obj.send(k.to_sym) != v
+          obj.send(k.to_sym).should.equal v
+        end
+      end
+    end
+  end
+end
+
+def xit(*args, &block)
+  puts "\nSKIPPING: #{args}"
+end
+
+def xdescribe(*args, &block)
+  puts "\nSKIPPING: #{args}"
+end
+
---
+++ spec/spec/more_spec.rb
@@ -0,0 +1,52 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+require 'ostruct'
+
+require 'spec/more'
+
+class MyClass 
+  attr_accessor :color, :height
+  def initialize(color, height)
+    @color = color
+    @height = height
+  end
+  
+  def each(&block)
+    [@color, @height].each(&block)
+  end
+end
+
+describe "spec more" do
+  before do
+    @obj1 = MyClass.new('blue', 52)
+    @obj2 = MyClass.new('blue', 52)
+    @obj3 = MyClass.new('red', 532)
+  end
+
+  it "has syntax like Test::More" do
+    @obj1.color.is 'blue' 
+    @obj1.isa MyClass
+    @obj1.color.isnt 'purple'
+    ok  @obj1.color == @obj2.color 
+
+    # and the negations
+    prcs = [lambda { @obj1.color.is 'green' }, 
+      lambda { @obj1.isa Hash },
+      lambda { @obj1.color.isnt 'blue' },
+      lambda { ok @obj1.color == @obj3.color }]
+    prcs.each do |prc|
+      prc.should.raise(Bacon::Error) 
+    end
+  end
+
+  it 'can check enumerable objects' do
+    @obj1.enums ['blue', 52]
+    @obj1.enums @obj2  # obj2 is also enumerable
+    lambda {@obj1.enums ['blue', 53]}.should.raise(Bacon::Error)
+  end
+
+  it 'can turn on and off specs and contexts' do
+    xit 'just skip this check for now' do end.should.be.nil
+    xdescribe 'just skip this context for now' do end.should.be.nil
+  end
+end
